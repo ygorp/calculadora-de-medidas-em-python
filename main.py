189072cce624eb1f156b650116ad8800c24f2722
Ygor Pacheco Nogueira <ygor-nogueira@hotmail.com>
2021-09-30 05:41:08 -0300

    cor janela

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from tkinter import ttk
 
 #cores
 cor1 = '#3b3b3b' #preta
+cor2 = '#ffffff' #branca
 
 janela = Tk()
 janela.title('')
@@ -13,13 +14,13 @@ janela.configure(bg=cor1)
 
 #---------------frames para janela---------------
 
-frame_cima = Frame(janela, width=450, height=50, bg='red', pady=0, padx=3, relief='flat')
+frame_cima = Frame(janela, width=450, height=50, bg=cor2, pady=0, padx=3, relief='flat')
 frame_cima.place(x=2, y=2)
 
-frame_esquerda = Frame(janela, width=450, height=220, bg='green', pady=0, padx=3, relief='flat')
+frame_esquerda = Frame(janela, width=450, height=220, bg=cor2, pady=0, padx=3, relief='flat')
 frame_esquerda.place(x=2, y=54)
 
-frame_direita = Frame(janela, width=198, height=260, bg='white', pady=0, padx=3, relief='flat')
+frame_direita = Frame(janela, width=198, height=260, bg=cor2, pady=0, padx=3, relief='flat')
 frame_direita.place(x=454, y=2)
 
 #-----------------------estilo para janela-------------------
